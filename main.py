74de710a2312b6f464fafd258ea120b6caaf3149
alexander <ahansen1335@gmail.com>
2023-11-19 10:03:41 +0100

    create main.py to abstract merge into an imported module

---
+++ main.py
@@ -0,0 +1,7 @@
+import merge
+
+def main():
+    merge.merge()
+
+if __name__ == '__main__':
+    main()
--- merge.py
+++ merge.py
@@ -1,14 +1,15 @@
 import os, sys
 from pypdf import PdfWriter
 
-merger = PdfWriter()
+def merge():
+    merger = PdfWriter()
 
-workingDirectory = input('Input desired directory to merge: ')
-os.chdir(workingDirectory)
+    workingDirectory = input('Input desired directory to merge: ')
+    os.chdir(workingDirectory)
 
-for pdf in os.listdir(workingDirectory):
-    merger.append(pdf)
+    for pdf in os.listdir(workingDirectory):
+        merger.append(pdf)
 
 
-merger.write(sys.argv[1])
-merger.close()
+    merger.write(sys.argv[1])
+    merger.close()
